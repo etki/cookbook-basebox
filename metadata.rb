fd1459a6ac38e9e053fb89a8d42f4fe649acb6a2
Etki <etki@etki.name>
2015-03-29 07:21:05 +0300

    Another hotfix, incorrect apt version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs and configures shared Basebox components'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'apt', '~> 2.7.0'
+depends 'apt', '~> 2.6.0'
 depends 'git', '~> 4.1.0'
 depends 'nginx', '~> 2.7.6'
 depends 'nginx_conf', '~> 0.0.5'
